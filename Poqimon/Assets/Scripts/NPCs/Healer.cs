74d23de83e3e1c6f062e14680fc995a013c913a8
bokdelajungla <bokdelajungla@hotmail.com>
2022-05-22 13:30:11 +0200

    Added Healing Music asset & logic

--- Poqimon/Assets/Scripts/NPCs/Healer.cs
+++ Poqimon/Assets/Scripts/NPCs/Healer.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Healer : MonoBehaviour
 {
+    [SerializeField] AudioClip healChime;
+
     public IEnumerator Heal(Transform player, Dialog dialog)
     {
         yield return DialogController.Instance.ShowDialogText("Your poqimons look tired! Let me take care of them");
 
+        AudioManager.i.PlaySfx(healChime, true);
         var playerParty = player.GetComponent<PoqimonParty>();
         playerParty.Party.ForEach(p => p.Heal());
         playerParty.PartyUpdated();
